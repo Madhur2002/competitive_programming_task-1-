b56161d38ab4f5f4738a67667266b0169fc3a791
Madhur2002 <93668517+Madhur2002@users.noreply.github.com>
2022-05-06 23:07:07 +0530

    uploaded file

---
+++ competitive programming task-1.cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+   char str[] = "A string.";
+   char *ptr = str;
+
+   cout <<str[0]<<' '<<*ptr<<' '<<ptr[3]<<endl;
+   ptr += 2;
+   cout <<*ptr<<' '<<ptr[2]<<' '<<ptr[5];
+
+   return 0;
+}
